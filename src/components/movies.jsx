9452ed2fdba919575c442451d4d7c49509a96532
mokuducu <m.okuducu04@gmail.com>
2019-06-10 12:14:09 +0100

    React movies add like example

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import {getMovies} from "../services/fakeMovieService";
+import Like from '../commons/like'
 
 class movies extends Component {
 
@@ -10,6 +10,14 @@ class movies extends Component {
     handleDelete = (movie) => {
        const movies=this.state.movies.filter(m=>m._id!==movie._id);
        this.setState({movies:movies})
+    };
+    handleLike=(movie)=>{
+        const movies=[...this.state.movies];
+        const  index=movies.indexOf(movie);
+        movies[index]={...movies[index]};
+        movies[index].liked=!movies[index].liked;
+        this.setState({movies});
+        //console.log(movies[index].liked);
     };
 
     render() {
@@ -27,15 +35,20 @@ class movies extends Component {
                             <th>Stock</th>
                             <th>Rate</th>
                             <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                    {this.state.movies.map(movie => (
+                    {
+                        this.state.movies.map(movie => (
                         <tr key={movie._id}>
                             <td>{movie.title}</td>
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <Like liked={movie.liked} onClick={()=>this.handleLike(movie)}/>
+                            </td>
                             <td>
                                 <button onClick={()=> this.handleDelete(movie)}
                                         className="btn btn-danger btn-sm">Delete
